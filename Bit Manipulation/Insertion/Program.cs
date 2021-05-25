24a2fec90cfcb79f56679ad0ff929b4c9c43a3b7
ShadyElia <shady.elia@hotmail.com>
2021-05-25 15:16:51 +0200

    finsih a Bit Manipulation problem

---
+++ Bit Manipulation/Insertion/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Insertion
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 1024;
+            PrintBinary("n", n);
+
+            int m = 19;
+            PrintBinary("m", m);
+
+            int i = 2, j = 6;
+
+            int result = Insertion(n, m, i, j);
+            PrintBinary("result", result);
+            Console.WriteLine("-------------");
+            n = 1201;
+            PrintBinary("n",n);
+
+            // in Binary M= 10011
+            m = 8;
+            PrintBinary("m",m);
+            i = 3; j = 6;
+            result = Insertion(n, m, i, j);
+            PrintBinary("result",result);
+        }
+
+        public static int Insertion(int n, int m, int i, int j)
+        {
+            int valueAfterShift = m << i;
+            int mask = FormatTheMast(i, j);
+            return (n & mask) | valueAfterShift;
+        }
+
+        public static int FormatTheMast(int i, int j)
+        {
+            int allOnes = ~0;
+            int left = allOnes << (j + 1);
+            int right = ((1 << i) - 1);
+            int mask = left | right;
+            return mask;
+        }
+
+        public static void PrintBinary(string name,int number)
+        {
+            string binary = Convert.ToString(number, 2);
+            Console.WriteLine(name +" : "+number + " in binary -> " + binary);
+        }
+    }
+}
---
+++ Graph/BST Sequences/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+	public static void Main()
+	{
+		Node node = new Node(4);
+		node.left = new Node(2);
+		node.left.left = new Node(1);
+		node.left.right = new Node(3);
+		node.right = new Node(6);
+		node.right.left = new Node(5);
+		node.right.right = new Node(8);
+
+		BstSequence bstSequence = new BstSequence();
+		List<List<int>> answer = bstSequence.GetList(node);
+		bstSequence.DisplaySequences(answer);
+	}
+}
+
+public class BstSequence
+{
+	public void DisplaySequences(List<List<int>> sequences)
+	{
+		Console.WriteLine("Sequences are : ");
+
+		for(int i=0;i<sequences.Count;i++)
+		{
+			string a = i+": ";
+			foreach (int s in sequences[i])
+			{
+				a += s + " => ";
+			}
+			Console.WriteLine(a);
+		}
+	}
+
+	public List<List<int>> GetList(Node root)
+	{
+		List<List<int>> results = new List<List<int>>();
+
+		if (root == null)
+		{
+			results.Add(new List<int>());
+			return results;
+		}
+
+		List<int> prefix = new List<int>();
+
+		prefix.Add(root.data);
+		Console.WriteLine(root.data);
+		List<List<int>> leftSeq = GetList(root.left);
+		List<List<int>> rightSeq = GetList(root.right);
+
+		foreach (List<int> first in leftSeq)
+			foreach (List<int> second in rightSeq)
+			{
+				List<List<int>> weaved = new List<List<int>>();
+				Weave(first, second, prefix, weaved);
+				results.AddRange(weaved);
+			}
+		return results;
+	}
+
+	private void Weave(List<int> first, List<int> second, List<int> prefix, List<List<int>> weaved)
+	{
+		if (first.Count == 0 || second.Count == 0)
+		{
+			List<int> result = prefix.GetRange(0, prefix.Count);
+			result.AddRange(first);
+			result.AddRange(second);
+
+			weaved.Add(result);
+			return;
+		}
+
+		int headFirst = first.First();
+		first.Remove(headFirst);
+		prefix.Add(headFirst);
+		Weave(first, second, prefix, weaved);
+		first.Add(headFirst);
+		prefix.Remove(headFirst);
+
+		int headSecond = second.First();
+		second.Remove(headSecond);
+		prefix.Add(headSecond);
+		Weave(first, second, prefix, weaved);
+		second.Add(headSecond);
+		prefix.Remove(headSecond);
+	}
+}
+
+public class Node
+{
+	public int data;
+	public Node left;
+	public Node right;
+
+	public Node(int d)
+	{
+		data = d;
+	}
+}
