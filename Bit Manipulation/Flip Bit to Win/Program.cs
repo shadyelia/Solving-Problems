ede96e2741261f479ab8d3cefd17fb7cc1d06e96
ShadyElia <shady.elia@hotmail.com>
2021-05-26 15:16:08 +0200

    solve some Bit Manipulation problems

---
+++ Bit Manipulation/Flip Bit to Win/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine(FlipBit(13));
+        Console.WriteLine(FlipBit(1775));
+        Console.WriteLine(FlipBit(15));
+	}
+	public static int FlipBit(int number)
+	{
+		string numberInBinary = Convert.ToString(number,2);
+		Console.WriteLine(numberInBinary);
+		int maxResult = 0 , currentResult = 0,prevZero =0;
+		bool firstZero = false;
+		
+		for(int i = 0;i<numberInBinary.Length;i++)
+		{
+			if(numberInBinary[i] == '0'){
+				if(firstZero){
+					firstZero = false;
+					if(currentResult > maxResult) maxResult = currentResult;
+					currentResult = i-prevZero;
+				}
+				else if (!firstZero){
+					firstZero = true;
+					prevZero = i;
+					currentResult++;
+				}
+			}
+			else if(numberInBinary[i] == '1'){
+				currentResult++;
+			}
+			if(i == numberInBinary.Length-1 && currentResult > maxResult){
+				maxResult = currentResult;
+			}		
+		}
+		
+		return maxResult;
+	}
+	
+	
+}
+
---
+++ Bit Manipulation/Next Number/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace Next_Number
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("------ number 5-----------");
+            GetNextNumbers(5);
+            Console.WriteLine("------ number 8-----------");
+            GetNextNumbers(8);
+        }
+
+        public static void GetNextNumbers(int number)
+        {
+            string numberInBinary = ConvertToBinary(number);
+            StringBuilder nextNumberInBinary = new StringBuilder(numberInBinary);
+            bool isOneFlipped = false; int length = numberInBinary.Length;
+            for (int i = length - 1; i >= 0; i--)
+            {
+                if (numberInBinary[i] == '1' && !isOneFlipped)
+                {
+                    nextNumberInBinary[i] = '0';
+                    isOneFlipped = true;
+                    if (i == 0)
+                    {
+                        nextNumberInBinary.Insert(0, '1');
+                    }
+                }
+                if (numberInBinary[i] == '0' && isOneFlipped)
+                {
+                    nextNumberInBinary[i] = '1';
+                    break;
+                }
+            }
+            Console.WriteLine("The next greatest  number : " + ConvertToInt(nextNumberInBinary) +" and binary => "+ nextNumberInBinary);
+            
+            nextNumberInBinary = new StringBuilder(numberInBinary);
+            isOneFlipped = false;
+            for (int i = 0; i < length ; i++)
+            {
+                if (numberInBinary[i] == '1' && !isOneFlipped)
+                {
+                    nextNumberInBinary[i] = '0';
+                    isOneFlipped = true;
+                }
+                if (numberInBinary[i] == '0' && isOneFlipped)
+                {
+                    nextNumberInBinary[i] = '1';
+                    break;
+                }
+            }
+            Console.WriteLine("The next smallest number : " + ConvertToInt(nextNumberInBinary) + " and binary => " + nextNumberInBinary);
+        }
+
+        private static string ConvertToBinary(int number)
+        {
+            return Convert.ToString(number, 2);
+        }
+        private static int ConvertToInt(StringBuilder number)
+        {
+            return Convert.ToInt32(number.ToString(), 2);
+        }
+    }
+}
