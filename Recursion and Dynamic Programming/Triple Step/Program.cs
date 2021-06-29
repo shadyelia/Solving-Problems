ed71ac7a0bac557f00ba94424ed2aabd111e1965
ShadyElia <shady.elia@hotmail.com>
2021-06-29 16:50:57 +0200

    add another way to triple step

--- Recursion and Dynamic Programming/Triple Step/Program.cs
+++ Recursion and Dynamic Programming/Triple Step/Program.cs
@@ -7,6 +7,7 @@ namespace Triple_Step
         static void Main(string[] args)
         {
             Console.WriteLine("N = 4 -> answer from FAP  : " + FindeAPath(4));
+            Console.WriteLine("N = 4 -> answer from FAP  : " + FindeAPath2(4));
         }
 
         static int FindeAPath(int n)
@@ -20,6 +21,20 @@ namespace Triple_Step
                 res[i] = res[i - 1] + res[i - 2] + res[i - 3];
             }
             return res[n];
+        }
+        static int FindeAPath2(int n)
+        {
+            if (n < 0) { return 0; }
+            if (n <= 1) { return 1; }
+            int[] res = { 1, 1, 2 };
+            for (int i = 3; i <= n; i++)
+            {
+                int count = res[0] + res[1] + res[2];
+                res[0] = res[1];
+                res[1] = res[2];
+                res[2] = count;
+            }
+            return res[2];
         }
     }
 }
