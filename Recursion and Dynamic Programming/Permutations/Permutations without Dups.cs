ed68ecd957085982189d3c0f864eb15729e34b67
ShadyElia <shady.elia@hotmail.com>
2021-07-09 21:39:09 +0200

    solve  2 problem  Permutations

---
+++ Recursion and Dynamic Programming/Permutations/Permutations without Dups.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Permutations
+{
+    class Permutations_without_Dups
+    {
+
+        static void Main()
+        {
+            string input = "abc";
+            List<string> result = GetPerm(input);
+            PrintResult(result);
+        }
+
+        public static List<string> GetPerm(string input)
+        {
+            int len = input.Length;
+            List<string> result = new List<string>();
+            List<string> prems = new List<string>();
+
+            if (len == 0)
+            {
+                result.Add("");
+                return result;
+            }
+
+            string first = input[0].ToString();
+            string remainder = input.Substring(1);
+            result = GetPerm(remainder);
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                for (int j = 0; j <= result[i].Length; j++)
+                {
+                    var s = result[i].Insert(j, first);
+                    prems.Add(s);
+                }
+            }
+            return prems;
+        }
+
+        public static void PrintResult(List<string> result)
+        {
+            foreach (var s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+    }
+}
---
+++ Recursion and Dynamic Programming/Permutations/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Permutations
+{
+    class Program
+    {
+        static void Main2()
+        {
+            string input = "aabbbbc";
+            List<string> result = GetPerm(input);
+            PrintResult(result);
+        }
+
+        public static List<string> GetPerm(string input)
+        {
+            Dictionary<char, int> map = BuildCharactersMap(input);
+            List<string> result = new List<string>();
+            GetPerm(map, "", input.Length, result);
+
+            return result;
+        }
+
+        private static void GetPerm(Dictionary<char, int> map, string prefix, int length, List<string> result)
+        {
+            if(length == 0)
+            {
+                result.Add(prefix);
+                return;
+            }
+            foreach(var c in map.Keys)
+            {
+                int count = map[c];
+                if (count > 0)
+                {
+                    map[c] = count - 1;
+                    GetPerm(map, prefix + c, length - 1, result);
+                    map[c] = count;
+                }
+            }
+        }
+
+        private static Dictionary<char, int> BuildCharactersMap(string input)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            foreach(char c in input)
+            {
+                if (map.ContainsKey(c))
+                {
+                    map[c] = map[c] + 1;
+                }
+                else
+                {
+                    map.Add(c, 1);
+                }
+            }
+            return map;
+        }
+
+        public static void PrintResult(List<string> result)
+        {
+            foreach(var s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+    }
+}
